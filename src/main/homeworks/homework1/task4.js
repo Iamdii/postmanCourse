759e49900fb4712c5dea21a263dc9318e207310c
Iamdii <azazarova99@mail.ru>
2021-07-10 13:47:19 +0300

    Resolved task 4

--- src/main/homeworks/homework1/task4.js
+++ src/main/homeworks/homework1/task4.js
@@ -5,10 +5,12 @@ Create object "utils". It should contain:
 - simple logger: has to contain 2 arguments - level (default value is "INFO"), message.
 */
 
+
+
 const utils = {
-  getRundomNumer: () => undefined,
-  getRandomString: () => undefined,
-  logger: () => undefined,
+  getRandomNumber: () => Math.random()*10,
+  getRandomString: () => Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5),
+  logger: (message, level="INFO") => console.log(`The level is ${level}. Message: ${message}.`),
 };
 
 module.exports = {
